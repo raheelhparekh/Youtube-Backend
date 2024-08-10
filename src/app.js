809b811d0bdd36615714174a61278632d81c4579
raheelhparekh <rhparekh2003@gmail.com>
2024-08-10 22:28:54 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,5 +14,11 @@ app.use(express.urlencoded({extended: true})); // url se data aata hai toh usse 
 app.use(express.static('public')); // public folder pdf folders vagera ko static folder bana diya
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.route.js';
 
+
+
+//routes declaration
+app.use('/api/v1/users', userRouter);  // http://localhost:3000/api/v1/users/register
 export {app}
